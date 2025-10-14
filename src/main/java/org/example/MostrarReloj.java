f4560bbb66b8e05b2eaf6dd52c3c56cf55aec92a
annelis sacalxot <annejysch@gmail.com>
2025-10-13 19:46:40 -0600

    Obtencion de hora actual correcta llamada desde main

--- src/main/java/org/example/MostrarReloj.java
+++ src/main/java/org/example/MostrarReloj.java
@@ -7,15 +7,24 @@ public class MostrarReloj {
     //atributos
     LocalTime horaActual = LocalTime.now();
     //utilizo horaActual para obtener los siguientes parametros
-    int hora =  horaActual.getHour();
+    int hora = horaActual.getHour();
     int minuto = horaActual.getMinute();
     int segundo = horaActual.getSecond();
 
     //metodos
-    /**getHoraActual, obtiene la hora actual de la computadora por medio
-     * de un arreglo de int**/
-    public int[] getHoraActual(){
+
+    /**
+     * getHoraActual, obtiene la hora actual de la computadora por medio
+     * de un arreglo de int
+     **/
+    public int[] getHoraActual() {
         return new int[]{hora, minuto, segundo};
+    }
+
+    /**getFormatoHoraActual manejo de un formato estandar para la hora**/
+    public String getFormatoHoraActual() {
+        // %02d= los espacios que tendran los numeros
+        return String.format("%02d:%02d:%02d", hora, minuto, segundo);
     }
 
 }
